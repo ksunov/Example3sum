9adc51f5ca2a5b9e31540b9cd7de3a26499c1163
Novikova Ksusha <sushi.91@mail.ru>
2023-01-27 17:06:39 +0700

    добавила рандом

--- Example003_sum/Program.cs
+++ Example003_sum/Program.cs
@@ -1,5 +1,8 @@
-﻿int numberA = 3;
-int numberB = 5;
-Console.WriteLine(numberA+numberB);
+﻿int numberA = new Random().Next(1, 100); //1 2 3...99
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1, 100); //1 2 3...99
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine(result);
 
 
